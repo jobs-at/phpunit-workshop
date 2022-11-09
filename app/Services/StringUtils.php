df3eab47d2f0b4d08a8cb4feb7d551e5da0e861f
Katharina Resch <katharina.resch@jobs.at>
2022-11-09 10:25:33 +0100

    Team Roman und Kathi

--- app/Services/StringUtils.php
+++ app/Services/StringUtils.php
@@ -12,6 +12,21 @@ class StringUtils
         return Str::lower(Str::reverse($str))=== Str::lower($str);
     }
 
+    public static function capitalize(string $str): string
+    {
+        return ucwords($str);
+    }
+
+    public static function addSpaceAfterEveryLetter(string $str): string
+    {
+        return rtrim(chunk_split($str, 1, ' '));
+    }
+
+    public static function removeGivenLetter(string $str, string $letter): string
+    {
+        return str_replace($letter, '', Str::lower($str));
+    }
+
     public static function capitalizeFirstLetter(string $word): string
     {
         return Str::ucfirst($word);
