c9f61d385f797c6646ad6b91236f6eb33fb1e0ba
Roman Sumereder <roman.sumereder@jobs.at>
2022-11-09 10:33:10 +0100

    Test

--- app/Services/StringUtils.php
+++ app/Services/StringUtils.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Str;
 
 class StringUtils
 {
+
     public static function isPalindrome(string $str): bool
     {
         return Str::lower(Str::reverse($str))=== Str::lower($str);
